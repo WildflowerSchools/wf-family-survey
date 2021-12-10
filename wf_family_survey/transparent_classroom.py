cc742595d8804fb522a8ecad650443a19f846476
Theodore Quinn <tcquinn@tcquinn.org>
2021-12-09 20:23:30 -0800

    Clean up field naming for network form template data

--- wf_family_survey/transparent_classroom.py
+++ wf_family_survey/transparent_classroom.py
@@ -14,18 +14,18 @@ class FamilySurveyTransparentClassroomClient(wf_core_data.TransparentClassroomCl
         self,
         template_name_re
     ):
-        network_template_data = self.fetch_network_form_template_data(format='dataframe')
+        network_form_template_data = self.fetch_network_form_template_data(format='dataframe')
         template_name_re_compiled = re.compile(template_name_re)
-        network_template_data['is_family_survey_network_template'] = network_template_data['network_template_name'].apply(
+        network_form_template_data['is_family_survey_template'] = network_form_template_data['form_template_name'].apply(
             lambda x: template_name_re_compiled.match(x) is not None
         )
-        network_template_data['is_family_survey_network_template'] = network_template_data['is_family_survey_network_template'].astype('bool')
-        network_template_data = network_template_data.reindex(columns=[
-            'network_template_name',
-            'is_family_survey_network_template'
+        network_form_template_data['is_family_survey_template'] = network_form_template_data['is_family_survey_template'].astype('bool')
+        network_form_template_data = network_form_template_data.reindex(columns=[
+            'form_template_name',
+            'is_family_survey_template'
         ])
-        network_template_data.sort_values('is_family_survey_network_template', ascending = False, inplace = True)
-        return network_template_data
+        network_form_template_data.sort_values('is_family_survey_template', ascending = False, inplace = True)
+        return network_form_template_data
 
     def fetch_family_survey_school_form_template_data(
         self,

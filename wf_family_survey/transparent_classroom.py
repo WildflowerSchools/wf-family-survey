1ecb3f887b192a0fb475aee4948dab2cfd474ada
Theodore Quinn <tcquinn@tcquinn.org>
2021-12-10 19:12:21 -0800

    Add ability to fetch family survey form data

--- wf_family_survey/transparent_classroom.py
+++ wf_family_survey/transparent_classroom.py
@@ -10,6 +10,41 @@ logger = logging.getLogger(__name__)
 
 class FamilySurveyTransparentClassroomClient(wf_core_data.TransparentClassroomClient):
 
+    def fetch_family_survey_form_data(
+        self,
+        field_name_lookup,
+        family_survey_school_form_template_ids=None,
+        family_survey_network_form_template_ids=None,
+        template_name_re=None,
+        school_ids=None
+    ):
+        if family_survey_school_form_template_ids is None:
+            logger.info('Family survey school form template IDs not specified. Fetching.')
+            family_survey_school_form_template_ids = self.fetch_family_survey_school_form_template_ids(
+                family_survey_network_form_template_ids=family_survey_network_form_template_ids,
+                template_name_re=template_name_re,
+                school_ids=school_ids
+            )
+        form_data=list()
+        for school_id, form_template_id in family_survey_school_form_template_ids:
+            logger.info('Fetching data for school ID {} and form template ID {}'.format(
+                school_id,
+                form_template_id
+            ))
+            form_data_school = self.fetch_form_data_school(
+                school_id=school_id,
+                form_template_id=form_template_id,
+                format='list'
+            )
+            for form_datum in form_data_school:
+                fields = form_datum.pop('form_fields')
+                for key, value in fields.items():
+                    form_datum[field_name_lookup[key]] = value
+                form_data.append(form_datum)
+        form_data = pd.DataFrame(form_data)
+        form_data.set_index(['school_id_tc', 'form_id_tc'], inplace=True)
+        return form_data
+
     def fetch_family_survey_school_form_template_ids(
         self,
         family_survey_network_form_template_ids=None,
